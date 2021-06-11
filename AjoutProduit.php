04d8d9fe68fbb07d15b81e131f98ec876e8a656b
Elouan <callec.elouan29@gmail.com>
2021-06-11 11:35:29 +0200

    upgrade ajoutProduit

--- AjoutProduit.php
+++ AjoutProduit.php
@@ -8,22 +8,44 @@
 </head>
 
 <body>
-    <form action="AjoutProduit.php" method="post">
-        <div>
-            <label for="prenom">
-                <span>Prénom :</span>
-            </label>
-            <input type="text" id="prenom" name="prenom">
-        </div>
-        <div class="centre">
-            <button type="submit">Valider</button>
-        </div>      
-    </form>
+    <div>
+        <form action="AjoutProduit.php" method="post">
+            <p>
+                <label for="motif">
+                    <span>Motif :</span>
+                </label>
+                <input type="text" id="motif" name="motif">
+            </p>
+            <p>
+                <label for="quantite">
+                    <span>Quantité :</span>
+                </label>
+                <input type="number" id="quantite" name="quantite">
+            </p>
+            <p>
+                Type de payement :<br/>
+                <input type="checkbox" name="cheque" id="cheque"><label for="cheque">Chèque</label><br/>
+                <input type="checkbox" name="chequeAptic" id="chequeAptic"><label for="chequeAptic">Chèque APTIC</label><br/>
+                <input type="checkbox" name="espece" id="espece"><label for="espece">Espèce</label><br/>
+                <input type="checkbox" name="compte" id="compte"><label for="compte">Compte</label>
+            </p>
+            <p>
+                <div class="centre">
+                    <button type="submit">Valider</button>
+                </div>
+            </p>
+                  
+        </form>
+    </div>
+    
 
     <p>Vous avez fourni les informations suivantes :</p>
 
     <?php
-        echo 'Prénom : '.$_POST["prenom"].'<br/>';
+        if(isset($_POST["prenom"])){
+            $prenom = $_POST["prenom"];
+            echo 'Prénom : '.$prenom.'<br/>';
+        }
     ?>
 </body>
 
