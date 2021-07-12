0789461d6e0ecc57eb42bcb17dd3a5e23ff0fb9e
Elouan <callec.elouan29@gmail.com>
2021-07-12 19:37:27 +0200

    finalisation de la requete d'ajout de crédit

--- ReqAjoutCredits.php
+++ ReqAjoutCredits.php
@@ -1,11 +1,21 @@
 <!-- Connexion a la base de donnees -->
 <?php
-include "connexionBDD.php"
+include "connexionBDD.php";
 
-// Modification du solde de l'utilisateur
-$req = $bdd->prepare("UPDATE utilisateurs SET util_solde = util_solde + :prix WHERE util_surnom = :surnom");
-$req->execute(array(
-    'prix' => $_POST['prix'],
-    'surnom' => $surnom));
+if(isset($_POST['surnom'])){
+    $req = $bdd->prepare("UPDATE utilisateurs SET util_solde = util_solde + :prix WHERE util_surnom = :surnom");
+    $req->execute(array(
+        'prix' => $_POST['prix'],
+        'surnom' => $_POST['surnom']
+    ));
+
+    if($req){
+        header('Location:index.php');
+    }
+    else{
+        echo "Un problème est survenu";
+    }
+}
+
 
 ?>
